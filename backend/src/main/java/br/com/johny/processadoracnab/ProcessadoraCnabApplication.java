a96aea45bb3cb8da86fc6fd25fecc1ae2e36f027
Johny Alencar <johnyguido@hotmail.com.br>
2023-11-08 21:56:48 -0300

    commit inicial projeto java 17 spring 3.1.5

---
+++ backend/src/main/java/br/com/johny/processadoracnab/ProcessadoraCnabApplication.java
@@ -0,0 +1,13 @@
+package br.com.johny.processadoracnab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessadoraCnabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessadoraCnabApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/johny/processadoracnab/ProcessadoraCnabApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.johny.processadoracnab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessadoraCnabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
