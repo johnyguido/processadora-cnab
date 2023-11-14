943e013f5c0abd3a9b998b24d8c245bb322dc3ec
Johny Alencar <johnyguido@hotmail.com.br>
2023-11-13 21:24:27 -0300

    criacao de entidades

---
+++ backend/src/main/java/br/com/johny/processadoracnab/entity/Transacao.java
@@ -0,0 +1,47 @@
+package br.com.johny.processadoracnab.entity;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+
+import java.math.BigDecimal;
+import java.sql.Time;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public record Transacao(
+        @Id Long id,
+        Integer tipo,
+        Date data,
+        BigDecimal valor,
+        Long cpf,
+        String cartao,
+        Time hora,
+        @Column("DONO_LOJA") String donoDaLoja,
+        @Column("NOME_LOJA") String nomeDaLoja) {
+
+    // Wither Pattern
+    public Transacao withValor(BigDecimal valor) {
+        return new Transacao(
+                id, tipo, data, valor, cpf,
+                cartao, hora, donoDaLoja, nomeDaLoja);
+    }
+
+    public Transacao withData(String data) throws ParseException {
+        var dateFormat = new SimpleDateFormat("yyyyMMdd");
+        var date = dateFormat.parse(data);
+
+        return new Transacao(
+                id, tipo, new Date(date.getTime()), valor, cpf,
+                cartao, hora, donoDaLoja, nomeDaLoja);
+    }
+
+    public Transacao withHora(String hora) throws ParseException {
+        var dateFormat = new SimpleDateFormat("HHmmss");
+        var date = dateFormat.parse(hora);
+
+        return new Transacao(
+                id, tipo, data, valor, cpf,
+                cartao, new Time(date.getTime()), donoDaLoja, nomeDaLoja);
+    }
+}
